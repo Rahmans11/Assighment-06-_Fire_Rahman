d15aac0fb8bab354269d9cea77607f5ebaf8cacc
Rahmans11 <2010631160071@student.unsika.ac.id>
2023-10-23 00:51:25 +0700

    Menambahkan fungsi matematika baru (perkalian dan pembagian)

--- Math_function.py
+++ Math_function.py
@@ -1,3 +1,12 @@
 
 def add(a, b):
     return a + b
+def multiply(a, b):
+    """Fungsi untuk mengalikan dua bilangan."""
+    return a * b
+
+def divide(a, b):
+    """Fungsi untuk membagi dua bilangan."""
+    if b == 0:
+        return "Tidak dapat membagi dengan nol."
+    return a / b
